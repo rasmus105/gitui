93d8c1798fb39f75bd009b3e855472e4403d9f58
Rasmus Kargo <rasmus105@protonmail.com>
2026-06-07 01:20:54 +0200

    initial render loop

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,37 @@
-fn main() {
-    println!("Hello, world!");
+use ratatui::{DefaultTerminal, Frame};
+
+struct App {
+    repo: gix::Repository,
+}
+
+impl App {
+    pub fn new(repo: gix::Repository) -> Self {
+        App { repo }
+    }
+}
+
+fn main() -> color_eyre::Result<()> {
+    color_eyre::install()?;
+
+    let repo = gix::discover(".")?;
+    let a = App::new(repo);
+
+    ratatui::run(|terminal| app(terminal, a))?;
+
+    Ok(())
+}
+
+fn app(terminal: &mut DefaultTerminal, mut app: App) -> std::io::Result<()> {
+    loop {
+        terminal.draw(|frame| render(frame, &mut app))?;
+
+        if crossterm::event::read()?.is_key_press() {
+            break Ok(());
+        }
+    }
+}
+
+fn render(frame: &mut Frame, app: &mut App) {
+    let text = format!("Git repo: {}\n", app.repo.path().display(),);
+    frame.render_widget(text, frame.area());
 }
